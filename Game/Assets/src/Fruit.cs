cff3b29321f5b25abd4ff40a2f21aff846efc455
mateusgpontes <mateus.gpontes@hotmail.com>
2021-02-23 19:54:29 -0300

    create apple score
    foi criado um score para o coletável maça(Apple), assim como as funções
    responsáveis para adicionar o total de maças coletadas, para assim o
    player poder ter uma sensação de pontuação no jogo.

---
+++ Game/Assets/src/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{   
+    public GameObject Collected;
+    public int Score;
+
+    private SpriteRenderer SpriteRenderer;
+    private CircleCollider2D CircleCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        CircleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            SpriteRenderer.enabled = false;
+            CircleCollider2D.enabled = false;
+            Collected.SetActive(true);
+
+            int score = GameController.instance.totalScore += Score;
+            GameController.instance.UpdateScoreText(score);
+            
+            Destroy(gameObject, 0.50f);
+        }
+    }
+}
